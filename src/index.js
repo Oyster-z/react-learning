2bd3dbea91573920865e00bb6917d79fe0c42e97
Oyster-z <switchyu@126.com>
2019-08-07 01:19:05 +0800

    lesson-03:03.create component way

--- src/index.js
+++ src/index.js
@@ -1,38 +1,84 @@
-import React, {Component} from 'react'
+import React, {
+    Component
+} from 'react'
 
-import { render } from 'react-dom' 
+import {
+    render
+} from 'react-dom'
 
-// 定义组件的第二种方式，使用类
+// const Header = () => <h1>类组件!!!</h1>
+// 嵌套，这是JSX的语法， 但不是合法的js代码
+// class App extends Component {
+//     render(){
+//         console.log(this.props.desc)
+//         return  (
+//         <div>
+//             <Header />
+//             <p>{this.props.desc}</p>
+//         </div>
+//         ) 
+
+//     }
+// }
+
+// 虚拟DOM 的表示方式
+// const appVDom = {
+//     tag: 'div',
+//     attrs: {
+//         className: 'app',
+//         id: 'aooRoot'
+//     },
+//     children: [{
+//         tag: 'h1',
+//         attrs: {
+//             className: 'title'
+//         },
+//         children: [
+//             'JSX原理'
+//         ]
+//     }, {
+//         tag: 'p',
+//         attrs: null,
+//         children: [
+//             '类组件是继承React.Component的'
+//         ]
+//     }]
+// }
+
+// 这里是使用类的形式创建的组件，所以react 在真正渲染的时候会把上面的代码编译为下面这个样子来运行，下面的代码就是合法的js代码
 class App extends Component {
-    render(){
-        console.log(this.props.desc)
-        return  (
-        <div>
-            <h1>类组件</h1>
-            <p>{this.props.desc}</p>
-        </div>
-        ) 
+    render() {
+        return (
+            // React.createElement 是一个方法，用于创建元素，可以有很多的参数，但是前两个是固定的;
+            // 第一个是 标签名
+            // 第二个可以理解为 标签的属性
+            // 剩下的，你就继续写更多的子元素吧
+            // React.createElement(type, [props], [....children])
+            React.createElement(
+                'div',
+                {
+                    className:'app',
+                    title:'appRoot'
+                },
+                React.createElement(
+                    'h1',
+                    {
+                        className:'title'
+                    },
+                    'JSX原理'
+                ),
+                React.createElement(
+                    'p',
+                    null,
+                    '类组件是继承React.Component的'
+                )
+            )
+        )
 
     }
 }
 
-
-// 原理
-// const app = new App({
-//     desc:"类组件是继承 React.Component的"
-// }).render()
-
-// <App desc="类组件是继承 React.Component的"/> 可以换成 app
-
-// 在react 16以前，使用这个方式来创建一个类组件
-// React.createClass({
-//     render(){
-//         return <h1>ZXXXX</h1>
-//     }
-// })
-
-// render 是react dom 提供的一个方法，这个方法通常只会用一次
-render(
-    <App desc="类组件是继承 React.Component的"/>,
+render( <
+    App desc = "类组件是继承 React.Component的" / > ,
     document.querySelector("#root")
 )
